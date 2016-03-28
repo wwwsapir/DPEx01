a01fe10fa22288fe6d5020357cf6f23cf5a2024c
wwwsapir <wwwsapir@gmail.com>
2016-03-28 20:47:48 +0300

    Initial Commit

---
+++ B16 Ex01 Sapir 201028867 Bar 200959286/MainForm.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace B16_Ex01_Sapir_201028867_Bar_200959286
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ButtonShowLikedPages = new System.Windows.Forms.Button();
+            this.ListBoxLikedPages = new System.Windows.Forms.ListBox();
+            this.LabelLikedPages = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TextBoxDaysToInactive = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ButtonShowLikedPages
+            // 
+            this.ButtonShowLikedPages.Location = new System.Drawing.Point(13, 13);
+            this.ButtonShowLikedPages.Name = "ButtonShowLikedPages";
+            this.ButtonShowLikedPages.Size = new System.Drawing.Size(169, 23);
+            this.ButtonShowLikedPages.TabIndex = 0;
+            this.ButtonShowLikedPages.Text = "Show My Inactive Liked Pages";
+            this.ButtonShowLikedPages.UseVisualStyleBackColor = true;
+            this.ButtonShowLikedPages.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ListBoxLikedPages
+            // 
+            this.ListBoxLikedPages.FormattingEnabled = true;
+            this.ListBoxLikedPages.Location = new System.Drawing.Point(12, 80);
+            this.ListBoxLikedPages.Name = "ListBoxLikedPages";
+            this.ListBoxLikedPages.Size = new System.Drawing.Size(397, 342);
+            this.ListBoxLikedPages.TabIndex = 1;
+            // 
+            // LabelLikedPages
+            // 
+            this.LabelLikedPages.AutoSize = true;
+            this.LabelLikedPages.Location = new System.Drawing.Point(10, 64);
+            this.LabelLikedPages.Name = "LabelLikedPages";
+            this.LabelLikedPages.Size = new System.Drawing.Size(316, 13);
+            this.LabelLikedPages.TabIndex = 2;
+            this.LabelLikedPages.Text = "Press the button to view the inactive pages you like on facebook.";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(328, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "A page will be counted as inactive if there aren\'t posts within the last";
+            // 
+            // TextBoxDaysToInactive
+            // 
+            this.TextBoxDaysToInactive.Location = new System.Drawing.Point(342, 40);
+            this.TextBoxDaysToInactive.Name = "TextBoxDaysToInactive";
+            this.TextBoxDaysToInactive.Size = new System.Drawing.Size(21, 20);
+            this.TextBoxDaysToInactive.TabIndex = 4;
+            this.TextBoxDaysToInactive.Text = "7";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(364, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "days.";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 429);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TextBoxDaysToInactive);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LabelLikedPages);
+            this.Controls.Add(this.ListBoxLikedPages);
+            this.Controls.Add(this.ButtonShowLikedPages);
+            this.Name = "MainForm";
+            this.Text = "s";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButtonShowLikedPages;
+        private System.Windows.Forms.ListBox ListBoxLikedPages;
+        private System.Windows.Forms.Label LabelLikedPages;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TextBoxDaysToInactive;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ B16 Ex01 Sapir 201028867 Bar 200959286/MainForm.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using FacebookWrapper;
+using FacebookWrapper.ObjectModel;
+
+namespace B16_Ex01_Sapir_201028867_Bar_200959286
+{
+
+    public partial class MainForm : Form
+    {
+        LoginResult m_user;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            FacebookService.s_CollectionLimit = 30;    // The current maximum allowed by facebook graph for pages is 100
+            m_user = FacebookService.Login("1085758691487251", "user_likes");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FacebookObjectCollection<Page> likedPagesCollection = m_user.LoggedInUser.LikedPages;
+            LabelLikedPages.Text = 
+                "You have " + likedPagesCollection.Count + " inactive liked pages on Facebook:";
+            FacebookObjectCollection<Post> latestPostsFromPage;
+            foreach (Page likedPage in likedPagesCollection)
+            {
+                latestPostsFromPage = likedPage.Posts;
+                bool isActive = false;
+                foreach (Post pagePost in latestPostsFromPage)
+                {
+                    isActive = false;
+                    int textBoxDaysToInactive;
+                    bool isDaysTextBocValueValid = int.TryParse(TextBoxDaysToInactive.Text, out textBoxDaysToInactive);
+                    if (!isDaysTextBocValueValid)
+                    {
+                        throw new NotImplementedException(); // TODO decide what to do in this case.
+                        //TODO Maybe message window that tells the user to choose 
+                    }
+                    if (pagePost.CreatedTime.GetValueOrDefault() >
+                        DateTime.Now - TimeSpan.FromDays(textBoxDaysToInactive))
+                    {
+                        isActive = true;
+                        break;
+                    }
+                }
+                if (!isActive)
+                {
+                    ListBoxLikedPages.Items.Add(likedPage.Name);
+                }
+            }
+            
+        }
+    }
+}
